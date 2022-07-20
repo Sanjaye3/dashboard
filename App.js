6bc6313a7ba2848a9f69fc2b66b69a5109088363
Sanjaye3 <92200293+Sanjaye3@users.noreply.github.com>
2022-07-20 14:56:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import ColorCard from './Components/ColorCard';
+import Cards from "C:\\Users\\SanjayeJ\\Desktop\\dashboard\\my-app\\src\\Components\\Cards.js";
+import Ill from "C:\\Users\\SanjayeJ\\Desktop\\dashboard\\my-app\\src\\Components\\Ill.js";
+import Login from "C:\\Users\\SanjayeJ\\Desktop\\dashboard\\my-app\\src\\Components\\Login.js";
+import Createac from "C:\\Users\\SanjayeJ\\Desktop\\dashboard\\my-app\\src\\Components\\Createac.js";
+
+function App() {
+  return (
+    <div className="App">
+        <Createac />
+      {/* <Login />  */}
+       {/* <Cards /> 
+       <ColorCard />
+       <Ill /> */}
+    </div>
+  );
+}
+
+export default App;
